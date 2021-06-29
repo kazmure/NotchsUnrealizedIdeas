4e3d6bd681fff3279561f11b7d6ac4b345e5efb9
kazmure <@DESKTOP-3MMK550>
2021-06-29 22:08:03 +0300

    ghfgfger

---
+++ src/main/java/net/mcreator/notchsunrealizedideas/block/GrassBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.notchsunrealizedideas.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.notchsunrealizedideas.NotchsUnrealizedIdeasModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NotchsUnrealizedIdeasModElements.ModElement.Tag
+public class GrassBlockBlock extends NotchsUnrealizedIdeasModElements.ModElement {
+	@ObjectHolder("notchs_unrealized_ideas_:grass_block")
+	public static final Block block = null;
+	public GrassBlockBlock(NotchsUnrealizedIdeasModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.harvestLevel(1).harvestTool(ToolType.SHOVEL).setRequiresTool());
+			setRegistryName("grass_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
