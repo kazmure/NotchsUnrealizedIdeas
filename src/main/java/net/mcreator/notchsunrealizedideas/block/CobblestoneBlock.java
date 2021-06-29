a40d5d71ef479ce5b270fbb2bd606ef41a2e1a1e
kazmure <@DESKTOP-3MMK550>
2021-06-29 22:14:22 +0300

    gfdfgdfg

---
+++ src/main/java/net/mcreator/notchsunrealizedideas/block/CobblestoneBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.notchsunrealizedideas.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.notchsunrealizedideas.NotchsUnrealizedIdeasModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NotchsUnrealizedIdeasModElements.ModElement.Tag
+public class CobblestoneBlock extends NotchsUnrealizedIdeasModElements.ModElement {
+	@ObjectHolder("notchs_unrealized_ideas_:cobblestone")
+	public static final Block block = null;
+	public CobblestoneBlock(NotchsUnrealizedIdeasModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("cobblestone");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/notchsunrealizedideas/block/LeavesBlockBlock.java
@@ -0,0 +1,50 @@
+
+package net.mcreator.notchsunrealizedideas.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.notchsunrealizedideas.NotchsUnrealizedIdeasModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NotchsUnrealizedIdeasModElements.ModElement.Tag
+public class LeavesBlockBlock extends NotchsUnrealizedIdeasModElements.ModElement {
+	@ObjectHolder("notchs_unrealized_ideas_:leaves_block")
+	public static final Block block = null;
+	public LeavesBlockBlock(NotchsUnrealizedIdeasModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(0f, 1f).setLightLevel(s -> 0).notSolid());
+			setRegistryName("leaves_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
